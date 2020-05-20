afaa5d1842bf91aff8c81c5d50d0a1cb2a1ce138
masterofnope <63602201+masterofnope@users.noreply.github.com>
2020-05-19 22:21:21 -0400

    more than items
    fixed jumping, added double+ jumps

--- CyberSlash/multi_jump.gd
+++ CyberSlash/multi_jump.gd
@@ -6,14 +6,15 @@ onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
 #onready var playernode = get_node("/root/Game/DemoRoom/TileMap2/Player")
 var picked_up = false
 
-func _on_Whetstone_body_entered(body):
+func _on_body_entered(body):
 	if picked_up == false:
 		picked_up = true
 		anim_player.play("pickup")
-		body.speed.x = body.speed.x + 100
-		print(body.speed.x)
+		body.multijumps = body.multijumps + 1
+		print(body.multijumps)
 	else:
 		print("nahfambudbuddyboi")
+
 
 
 
--- CyberSlash/src/Objects/Multi_jump.gd
+++ CyberSlash/src/Objects/Multi_jump.gd
@@ -6,12 +6,12 @@ onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
 #onready var playernode = get_node("/root/Game/DemoRoom/TileMap2/Player")
 var picked_up = false
 
-func _on_Whetstone_body_entered(body):
+func _on_Multi_jump_body_entered(body):
 	if picked_up == false:
 		picked_up = true
-		anim_player.play("pickup")
-		body.speed.x = body.speed.x + 100
-		print(body.speed.x)
+		anim_player.play("pickup_multi_jump")
+		body.maxmultijumps = body.maxmultijumps + 1
+		print(body.multijumps)
 	else:
 		print("nahfambudbuddyboi")
 
