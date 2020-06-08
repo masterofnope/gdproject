7f2c32dc193d6576baf18d5e32519cd357d916a0
HJYlitalo <50092368+HJYlitalo@users.noreply.github.com>
2020-06-08 13:41:20 -0400

    Revert "Revert "Revert "Revert "Merge branch 'master' into LevelDesign""""
    This reverts commit 819acf6d3395074b32a2072dab054a639430e2d6.

--- CyberSlash/src/Game.gd
+++ CyberSlash/src/Game.gd
@@ -5,7 +5,7 @@ extends Node
 
 # The "_" prefix is a convention to indicate that variables are private,
 # that is to say, another node or script should not access them.
-onready var _pause_menu = $InterfaceLayer/PauseMenu
+#onready var _pause_menu = $InterfaceLayer/PauseMenu
 
 
 func _init():
@@ -21,11 +21,11 @@ func _input(event):
 	# when the game is paused, so this code keeps running.
 	# To see that, select GlobalControls, and scroll down to the Pause category
 	# in the inspector.
-	elif event.is_action_pressed("toggle_pause"):
-		var tree = get_tree()
-		tree.paused = not tree.paused
-		if tree.paused:
-			_pause_menu.open()
-		else:
-			_pause_menu.close()
-		get_tree().set_input_as_handled()
+	#elif event.is_action_pressed("toggle_pause"):
+	#	var tree = get_tree()
+	#	tree.paused = not tree.paused
+	#	if tree.paused:
+	#		_pause_menu.open()
+	#	else:
+	#		_pause_menu.close()
+	#	get_tree().set_input_as_handled()
