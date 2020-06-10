e1972b35675748f39f35b95ce6019f9c5a752aa2
Jordyn Marlow <jordynmarlow@gmail.com>
2020-06-10 10:19:16 -0400

    Spawners activate when player is in range

--- CyberSlash/Enemies/BasicBotSpawner.gd
+++ CyberSlash/Enemies/BasicBotSpawner.gd
@@ -8,9 +8,11 @@ const spawn_time = 5 # number of seconds between enemies spawning
 var num_bots = 0 # number of basic bots in scene
 var timer = Timer.new()
 var bot_type = load("res://Enemies/BasicBot.tscn")
+onready var player = get_parent().get_parent().get_node("Player")
 var bots = []
 var min_x
 var max_x
+var activated = false
 
 func set_platform_bounds(min_bound, max_bound):
 	min_x = min_bound
@@ -22,13 +24,18 @@ func _ready():
 	self.add_child(timer)
 	timer.set_wait_time(spawn_time)
 	timer.connect("timeout", self, "spawn_bot")
-	timer.start()
+
+func _process(delta):
+	if abs(player.position.x - $BasicBotSpawner.global_position.x) < 500 and abs(player.position.y - $BasicBotSpawner.global_position.y) < 500 and not activated:
+		print("activate spawner")
+		activated = true
+		timer.start()
 
 func spawn_bot():
 	var bot = bot_type.instance()
 	bot.position.x = $BasicBotSpawner.position.x
 	bot.position.y = $BasicBotSpawner.position.y - (bot.get_node("BotArea/CollisionShape2D").shape.extents.y / 2) - 5
-	bot.set_player_instance(get_parent().get_parent().get_node("Player"))
+	bot.set_player_instance(player)
 	bot.connect("killed",get_parent().get_parent(),"_on_BasicBot_killed")
 	bot.set_platform_bounds(min_x, max_x)
 	add_child(bot)
