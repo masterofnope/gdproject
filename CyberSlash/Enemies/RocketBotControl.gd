0231e81ffa9e034465dce9cd77f86666f5f483be
weir6360 <weir6360@kettering.edu>
2020-06-09 20:25:17 -0400

    Player and Bot Health Bars
    HealthBar for Player and Bots

--- CyberSlash/Enemies/RocketBotControl.gd
+++ CyberSlash/Enemies/RocketBotControl.gd
@@ -6,6 +6,9 @@ extends KinematicBody2D
 signal health_updated(health)
 signal killed()
 
+onready var effects_animation = $DamageAnimationPlayer
+onready var health_bar = $HealthBar
+
 const vert_pos_thresh = 10 # vertical threshold for enemy being in range of player
 const hori_pos_thresh = 100 # horizontal threshold for enemy being in range of player
 const ambush_dist = 50 # distance enemy stops from player during ambush
@@ -23,13 +26,14 @@ var attack_timer = Timer.new()
 var player
 export var state = "friendly" # attack, chase, idle
 var walking_state = "run" # run or idle
-export var max_health = 2
+export var max_health = 3
 onready var health = max_health setget _set_health
 
 func damage(object):
 	if object.name == "Area2D":
 		print("hit by player")
 		_set_health(health - 1)
+		effects_animation.play("damage")
 		# modulate is pink: 255, 145, 145
 		# after 1 second, set modulate back to white
 		#$DamagedTimer.start()
@@ -47,6 +51,7 @@ func despawn():
 func _set_health(value):
 	var prev_health = health
 	health = clamp(value, 0, max_health)
+	health_bar._on_health_updated(health)
 	if health != prev_health:
 		emit_signal("health_updated", health)
 		if health == 0:
@@ -102,6 +107,7 @@ func face_player():
 		switch_direction()
 
 func _ready():
+	health_bar._on_max_health_updated(3)
 	start_walk()
 	attack_timer.set_one_shot(true)
 	self.add_child(attack_timer)
@@ -110,6 +116,7 @@ func _ready():
 	$DamagedTimer.connect("timeout", self, "clear_modulate")
 	$DespawnTimer.connect("timeout", self, "despawn")
 	$BotArea.connect("area_entered", self, "damage")
+
 
 func _process(_delta):
 	rand.randomize()
@@ -155,4 +162,5 @@ func _process(_delta):
 func _physics_process(delta):
 	velocity = move_and_slide(velocity * delta)
 
-
+func _on_DamagedTimer_timeout():
+	effects_animation.play("rest")
--- CyberSlash/src/Actors/Player.gd
+++ CyberSlash/src/Actors/Player.gd
@@ -6,6 +6,7 @@ const FLOOR_DETECT_DISTANCE = 15.0
 export(String) var action_prefix = ""
 
 signal health_updated(health)
+signal timeout()
 signal killed()
 
 export (float) var max_health = 5
@@ -21,19 +22,19 @@ onready var attack_anim = $Body/AttackPlayer
 onready var attack_sound = $SlashSound
 onready var attack_cooldown = $SlashAnimation
 onready var attack_box = $Area2D/AttackHitbox
+onready var health_bar = $HealthBar
 
 onready var multijumps = 0
 onready var maxmultijumps = 0
 onready var is_hor_slashing = false
-
-var format_string = "HP: %s / %s"
-var actual_string = format_string % [current_health, max_health]
 
 func _ready():
 	var camera: Camera2D = $Camera
 	if action_prefix == "p_":
 		camera.custom_viewport = $"../.."
 	attack_cooldown.connect("timeout",self,"_on_attack_cooldown_end")
+	invul_timer.connect("timeout", self, "_on_InvulnerabilityTimer_timeout")
+	health_bar._on_max_health_updated(max_health)
 
 func reset_jumps(doordonot):
 	if doordonot:
@@ -53,29 +54,15 @@ func _physics_process(_delta):
 		_velocity, snap_vector, FLOOR_NORMAL, not is_on_platform, 4, 0.9, false
 	)
 
-	# When the character’s direction changes, we want to to scale the Sprite accordingly to flip it.
-	# This will make Robi face left or right depending on the direction you move.
 	if direction.x != 0:
 		sprite.scale.x = 1 if direction.x > 0 else -1
-
-	# We use the sprite's scale to store Robi’s look direction which allows us to shoot
-	# bullets forward.
-	# There are many situations like these where you can reuse existing properties instead of
-	# creating new variables.
+		
 	if Input.is_action_just_pressed(action_prefix + "p_attack_horizontal") and is_hor_slashing == false:
 		is_hor_slashing = true
 		attack_cooldown.start()
 		attack_anim.play("slash")
 		attack_sound.play()
 		
-
-
-	#var animation = get_new_animation(is_hor_slashing)
-	#if animation != animation_player.current_animation and slash_timer.is_stopped():
-	#	animation_player.play(animation)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 func _on_attack_cooldown_end():
 	is_hor_slashing = false
@@ -121,16 +108,19 @@ func get_new_animation(is_shooting = false):
 	return animation_new
 
 func damage(damage_amount):
+	invul_timer.start()
+	invul_timer.stop()
 	if invul_timer.is_stopped():
 		invul_timer.start()
 		_set_health(current_health - damage_amount)
 		effects_animation.play("damage")
 		effects_animation.queue("flash")
+		#effects_animation.queue("rest")
 
 func _set_health(value):
 	var prev = current_health
 	current_health = clamp(value, 0, max_health)
-	print(current_health)
+	health_bar._on_health_updated(current_health)
 	if current_health != prev:
 		emit_signal("health_updated", current_health)
 		if current_health == 0: 
@@ -139,8 +129,9 @@ func _set_health(value):
 func kill ():
 	print("player killed")
 	emit_signal("killed")
-	pass #kill_me
+
 
 
 func _on_InvulnerabilityTimer_timeout():
 	effects_animation.play("rest")
+	print("Invul Timer Timed Out")
